01331582fac7cacc34cc46d2c2c19ca99a2f1302
colindouglas <colin.douglas@cds-snc.ca>
2026-07-21 12:58:34 -0300

    fix: add tag to resolve Conftest warning

--- infra/oidc.tf
+++ infra/oidc.tf
@@ -20,9 +20,7 @@ resource "aws_iam_policy" "plan_state" {
   name        = "service-inventory-lookup-plan-state"
   description = "State bucket access for the service-inventory-lookup plan role."
   policy      = data.aws_iam_policy_document.plan_state.json
-  tags = {
-    CostCentre = "service-inventory-lookup"
-  }
+  tags = local.common_tags
 }
 
 locals {
